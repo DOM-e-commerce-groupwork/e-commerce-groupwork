dd15244e4ac99f7180c3100b678c6814b1e9de11
Marina294 <69162947+Marina294@users.noreply.github.com>
2021-02-10 15:43:55 -0800

    added cart html and some css

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 (function(){
-const cart-info
-
 
 })();
 
+
+
+<div class="item-text">
+<h3 id="cart-item-title"> ${item.name}</h3>
+<span>$</span>
+<span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
+</div>
+<a href="#" id="cart-item-remove" class="cart-item-remove">
+<img src="./img/delete.svg">
+</a>
+</div>
