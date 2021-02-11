2da32ba938c8ab700aa4482a690317851ba6c2a5
Marina Braholka <ma.braholka@gmail.com>
2021-02-10 18:43:04 -0800

    Add item to cart, multiply their numbers and delete them from cart

--- js/app.js
+++ js/app.js
@@ -1,15 +1,51 @@
-(function(){
+let products = []
 
-})();
+const handleCartList = () => {
+  const list = products.map(li => `<li>
+    <p class="product-name">${li.name}</p>
+    <p class="product-price">$ ${li.price.toFixed(2)}</p>
+    <span class="product-count">X ${li.count}</span>
+  </li>`)
+  
+  document.getElementById("cartboy").innerHTML = products.length > 0 ? list.join("") : `<li></li>`;
+}
 
+const handleTotal = () => {
+  const total = products.reduce((acc, cur) => acc += parseFloat(cur.price), 0)
 
+  document.getElementById("cart-total").innerHTML = products.length > 0 ? `$ ${total.toFixed(2)}` : "Empty cart";
+}
 
-<div class="item-text">
-<h3 id="cart-item-title"> ${item.name}</h3>
-<span>$</span>
-<span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
-</div>
-<a href="#" id="cart-item-remove" class="cart-item-remove">
-<img src="./img/delete.svg">
-</a>
-</div>
+const handleProducts = (e) => {
+  const product = {
+    name: e.parentNode.querySelectorAll(".products-name") && e.parentNode.querySelectorAll(".products-name")[0].innerText,
+    originalPrice: e.parentNode.querySelectorAll(".products-name") && parseFloat(e.parentNode.querySelectorAll(".products-price")[0].innerText.split("$ ")[1]),
+    price: e.parentNode.querySelectorAll(".products-name") && parseFloat(e.parentNode.querySelectorAll(".products-price")[0].innerText.split("$ ")[1]),
+    count: 1
+  }
+
+  const p0 = products.map(p => p.name === product.name ? ({ 
+    ...p,
+    count: p.count += 1,
+    price: p.originalPrice * p.count
+  }) : p)
+
+  products = products.find(p => p.name === product.name) ? p0 : [ ...products, product ]
+}
+
+const addToCart = (e) => {
+  handleProducts(e)
+  handleCartList()
+  handleTotal()
+}
+
+const clearCart = () => {
+  products = []
+  handleCartList()
+  handleTotal()
+}
+
+const toggleDisplayCart = () => {
+  var cart = document.getElementById('cart') // Using a class instead, see note below.
+  cart.classList.toggle('show-cart');
+}
